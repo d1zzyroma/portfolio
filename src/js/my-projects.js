ecf65fa56bd5ebbdcc6c5d4edd4197f964d8092a
d1zzyroma <nataliawologin@gmail.com>
2025-02-22 15:26:26 +0100

    123

---
+++ src/js/my-projects.js
@@ -0,0 +1,128 @@
+import energy from '../img/My-project/js-jpg/energy.jpg'
+import energyRetina from "../img/My-project/js-jpg/energy-2x.jpg"
+import fruitbox from "../img/My-project/js-jpg/fruitbox.jpg"
+import fruitboxRetina from "../img/My-project/js-jpg/fruitbox-2x.jpg"
+import jewelry from "../img/My-project/js-jpg/jewelry.jpg"
+import jewelryRetina from "../img/My-project/js-jpg/jewelry-2x.jpg"
+import mimino from "../img/My-project/js-jpg/mimino.jpg"
+import miminoRetina from "../img/My-project/js-jpg/mimino-2x.jpg"
+import power from "../img/My-project/js-jpg/power.jpg"
+import powerRetina from "../img/My-project/js-jpg/power-2x.jpg"
+import starlight from "../img/My-project/js-jpg/starlight.jpg"
+import starlightRetina from "../img/My-project/js-jpg/starlight-2x.jpg"
+import vyshyvanka from "../img/My-project/js-jpg/vyshyvanka.jpg"
+import vyshyvankaRetina from "../img/My-project/js-jpg/vyshyvanka-2x.jpg"
+import icon from "../img/symbol-defs.svg"
+
+
+
+const arr = [
+    {
+        src: `${energy}`,
+        retina: `${energyRetina}`,
+        alt: "Energy Flow Webservice"
+    },
+    {
+        src: `${fruitbox}`,
+        retina: `${fruitboxRetina}`,
+        alt: "fruitbox online store"
+    },
+    {
+        src: `${jewelry}`,
+        retina: `${jewelryRetina}`,
+        alt: "chego jewelry website"
+    },
+    {
+        src: `${mimino}`,
+        retina: `${miminoRetina}`,
+        alt: "mimino website"
+    },
+    {
+        src: `${power}`,
+        retina: `${powerRetina}`,
+        alt: "power pulse webservice"
+    },
+    {
+        src: `${starlight}`,
+        retina: `${starlightRetina}`,
+        alt: "starlight studio landing page"
+    },
+    {
+        src: `${vyshyvanka}`,
+        retina: `${vyshyvankaRetina}`,
+        alt: "vyshyvanka vibes Landing Page"
+    },
+]
+
+const btn = document.querySelector(".load-more-btn");
+const imageList = document.querySelector(".image-list");
+
+let index = 0;
+
+btn.addEventListener("click", handleClick); 
+
+function handleClick() {
+    loadImages();
+}
+
+function loadImages () {
+    for (let i = 0; i < 3; i++) {
+        if (index >= arr.length) {
+            btn.style.display = "none";
+            return;
+        }
+        imageList.insertAdjacentHTML("beforeend", renderImage(arr[index]))
+        index++;
+    }
+    
+
+}
+
+function renderImage(array) {
+    return `
+            <li class="image-item">
+        
+          <picture>
+            <source
+              media="(min-width: 1280px)"
+              srcset="
+                ${array.src} 1x,
+                ${array.retina} 2x
+              "
+            />
+            <source
+              media="(min-width: 768px)"
+              srcset="
+                ${array.src} 1x,
+                ${array.retina} 2x
+              "
+            />
+            <source
+              media="(max-width: 767px)"
+              srcset="
+                ${array.src} 1x,
+                ${array.retina} 2x
+              "
+            />
+            <img
+              src="${array.src}"
+              alt="${array.alt}"
+              width="1008"
+              height="578"
+              class="img"
+            />
+          </picture>
+        
+        <p class="technologies">React, JavaScript, Node JS, Git</p>
+        <div class="link-wrapper">
+          <p class="img-naming">${array.alt}</p>
+          <a class="visit-link" href="https://github.com/BigBlackJoke/Portfolio-Group-13" target="_blank">
+            Visit
+            <svg width="24" height="24">
+              <use href="${icon}#icon-visit"></use>
+            </svg>
+          </a>
+        </div>
+      </li>
+        `
+}
---
+++ src/js/work-together.js
@@ -0,0 +1,59 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+export function handleForm() {
+  const contactForm = document.querySelector(".work-together__form");
+  contactForm.addEventListener("submit", handleSubmit);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    const url = "https://portfolio-js.b.goit.study/api/requests";
+    const requestData = {
+      email: email,
+      comment: message,
+    };
+    // const pageBody = document.querySelector('body');
+    const modal = document.querySelector('.work-together__backdrop');
+    const closeModalBtn = document.querySelector('.modal__close');
+    const modalBackdrop = document.querySelector('.work-together__backdrop');
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        modal.classList.add('active');
+        document.body.style.overflow = 'hidden';
+        closeModalBtn.addEventListener('click', () => {
+          modal.classList.remove('active');
+          document.body.style.overflow = 'visible';
+        });
+        modalBackdrop.addEventListener('click', (event) => {
+          if(event.target===event.currentTarget) {
+            modal.classList.remove('active');
+            document.body.style.overflow = 'visible';
+          }
+        });
+        contactForm.reset();
+      })
+      .catch((error) => {
+        iziToast.error({
+          title: 'Error',
+          message: error.message,
+          backgroundColor: '#de4f69',
+          iconColor: '#ffffff',
+          messageColor: '#fff',
+        });
+      });
+  }
+}
+handleForm();
