a4f4c700e4a196a7d9b8e91d4fc17f22b84721a1
d1zzyroma <nataliawologin@gmail.com>
2025-05-27 21:32:09 +0200

    123

--- src/main.js
+++ src/main.js
@@ -3,6 +3,36 @@ import './js/faq';
 import './js/header';
 import './js/reviews';
 import './js/work-together';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const accordion = new Accordion('.accordion-container');
+
+const firstItem = document.querySelector('.ac:first-child');
+
+firstItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)';
+firstItem.style.borderRadius = '16px';
+
+accordion.open(0);
+
+let lastOpenedItem = firstItem;
+
+const btnEl = document.querySelectorAll('.ac-trigger');
+btnEl.forEach(element => {
+    element.addEventListener('click', () => {
+        const currentItem = element.closest('.ac');
+
+        if (lastOpenedItem !== currentItem) {
+            lastOpenedItem.style.backgroundColor = '';
+            lastOpenedItem.style.borderRadius = '';
+        }
+
+        currentItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)';
+        currentItem.style.borderRadius = '16px'; 
+
+        lastOpenedItem = currentItem;
+    });
+});
 
 const darkBtn = document.querySelector('.night-mode-checkbox');
 const sections = document.querySelectorAll('section');
